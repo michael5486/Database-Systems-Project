88a0825be91186df400276289e33df9beba21e15
michael5486 <michael5486@gmail.com>
2016-03-23 17:57:09 -0400

    done for the day

--- Pollo Por Favor/public_html/login.js
+++ Pollo Por Favor/public_html/login.js
@@ -1,3 +1,9 @@
 $('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+$(".login_page").click(function() {
+    $("#content").animate(
+            {"width": "500px"},
+            "fast");
+});
