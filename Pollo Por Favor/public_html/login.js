8fe63f71286d269266c2e678b76824bb81fd08ef
michael5486 <michael5486@gmail.com>
2016-04-01 10:28:18 -0400

    aboutUs

--- Pollo Por Favor/public_html/login.js
+++
@@ -1,9 +0,0 @@
-$('.message').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
-
-$(".login_page").click(function() {
-    $("#content").animate(
-            {"width": "500px"},
-            "fast");
-});
