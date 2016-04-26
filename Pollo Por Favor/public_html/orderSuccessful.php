fa97581f0781e0946b1a5a545383c0407d727d95
Paul Stoner <paulstonerjr@gmail.com>
2016-04-25 19:19:29 -0500

    temp1, paymentPage, orderSuccessful page

---
+++ Pollo Por Favor/public_html/orderSuccessful.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Order Successful!</title>
+        <link rel="stylesheet" type="text/css" href="css/orderPage.css">
+    </head>
+    <body>
+        <div id="header">
+            <table class="header_table">
+                <tr>
+                    <td class="col_header_logo"> 
+                        <a href="index.html">
+                            <img src="pix/PPF_logo.png" alt="Pollo Por Favor logo" width="80" height="80">
+                        </a>
+                    </td>
+                    <td class="col_header_address">
+                        <p>2140 L Street, NW
+                            <br>
+                            Washington, DC 20037<p>        
+                    </td>
+                    <td class="col_header_username">
+                        <p>Username</p>
+                    </td>
+                </tr>
+            </table>
+            <br>
+        </div>
+    </head>
+    <body>
+        <div>TODO write content</div>
+    </body>
+</html>
