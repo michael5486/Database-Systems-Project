4ba7167ab88d6fb85da96f10e10c17404c640a0b
Paul Stoner <paulstonerjr@gmail.com>
2016-04-26 19:57:49 -0500

    functional paymentPage

---
+++ Pollo Por Favor/public_html/paymentPage.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function () {
+        
+    $("#Visa").click(function () {
+        $('#drop_button').text('Visa');
+    });
+    
+    $("#Master").click(function () {
+        $('#drop_button').text('Master');
+    });    
+    
+    $("#American_Express").click(function () {
+        $('#drop_button').text('American Express');
+    });    
+});
